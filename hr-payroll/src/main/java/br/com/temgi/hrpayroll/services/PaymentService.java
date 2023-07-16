23fe60e4352160c93ecf42653a5516f2f140c3a2
Rui Temgi <ruisaraivasp@gmail.com>
2023-07-16 13:08:56 -0300

    Implementação SpringCloud FeignClient

--- hr-payroll/src/main/java/br/com/temgi/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/temgi/hrpayroll/services/PaymentService.java
@@ -1,32 +1,22 @@
 package br.com.temgi.hrpayroll.services;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 import br.com.temgi.hrpayroll.entities.Payment;
 import br.com.temgi.hrpayroll.entities.Worker;
+import br.com.temgi.hrpayroll.feignclients.WorkerFeignClient;
 
 
 @Service
 public class PaymentService {
 
-	@Value("${hr-worker.host}")
-	private String workerHost;
-
 	@Autowired
-	private RestTemplate restTemplate;
+	private WorkerFeignClient workerFeignClient;
 
 	public Payment getPayment(long workerId, int days) { 
 		
-		Map<String, String> uriVariables = new HashMap<>();
-		uriVariables.put("id", ""+workerId);
-
-		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables); 
+		Worker worker = workerFeignClient.findById(workerId).getBody();
 		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
