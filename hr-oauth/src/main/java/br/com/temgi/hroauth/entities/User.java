d94a24f9ab949907bb6cc278000d30cdfdf3b50c
Rui Temgi <ruisaraivasp@gmail.com>
2023-07-18 19:22:05 -0300

    Configuração do SpringSecurity com JWT

--- hr-oauth/src/main/java/br/com/temgi/hroauth/entities/User.java
+++ hr-oauth/src/main/java/br/com/temgi/hroauth/entities/User.java
@@ -1,21 +1,25 @@
 package br.com.temgi.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class User implements Serializable{
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
 
 	private Set<Role> roles = new HashSet<>();
-	
+
 	public User() {}
 
 	public User(Long id, String name, String email, String password) {
@@ -25,7 +29,6 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	
 	public Long getId() {
 		return id;
 	}
@@ -57,15 +60,17 @@ public class User implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
 
-	
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -77,6 +82,44 @@ public class User implements Serializable{
 		if (getClass() != obj.getClass())
 			return false;
 		User other = (User) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName())).collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 }
--- hr-oauth/src/main/java/br/com/temgi/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/br/com/temgi/hroauth/services/UserService.java
@@ -3,14 +3,17 @@ package br.com.temgi.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.temgi.hroauth.entities.User;
 import br.com.temgi.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
-	
+public class UserService implements UserDetailsService {
+
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
@@ -18,11 +21,22 @@ public class UserService {
 	
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
-		if(user == null) {
-			logger.error("Email Not Found: " + email);
-			throw new IllegalArgumentException("Email Não Encontrado");
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found");
 		}
-		logger.info("Email Found: " + email);
+		logger.info("Email found: " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
